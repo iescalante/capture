f0b874b66ad49f1455bab159f3272e5a574e71bb
Ivan Escalante <ivan-enrique.escalante-cornejo@ibm.com>
2021-05-11 12:51:52 -0400

    added different paths

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from "react";
 import AboutUs from "./pages/AboutUs";
+import GlobalStyle from "./components/GlobalStyle";
+import Nav from "./components/Nav";
+import OurWork from "./pages/OurWork";
+import ContactUs from "./pages/ContactUs";
+import MovieDetail from "./pages/MovieDetail";
+import { Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="App">
-      <AboutUs />
+      <GlobalStyle />
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work" exact>
+          <OurWork />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail />
+        </Route>
+        <Route path="/contact" exact>
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 };
